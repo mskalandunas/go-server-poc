a9d51adff9e9c3dac6a9e1068d582ebf48357fd6
mskalandunas <mskalandunas@gmail.com>
2022-04-16 22:00:05 -0700

    add mock nofitication endpoint

---
+++ README.md
@@ -0,0 +1,5 @@
+If `go` is not already installed, [install it using the instructions that suit your system](https://go.dev/doc/install).
+
+`go install`
+
+`go run server.go`
--- server.go
+++ server.go
@@ -6,12 +6,27 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Notification struct {
+	Body string `json:"body"`
+	Href string `json:"name"`
+	Icon string `json:"icon"`
+}
+
+func handleGetNotifications(c echo.Context) error {
+	return c.JSON(http.StatusOK, &Notification{
+		Body: "Text",
+		Href: "https://google.com",
+		Icon: "ICON_IDENTIFIER",
+	})
+}
+
 func handleGet(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	return c.JSON(http.StatusOK, "Hello world!")
 }
 
 func main() {
 	e := echo.New()
-	e.GET("/", handleGet)
+	e.GET("/ping", handleGet)
+	e.GET("notifications", handleGetNotifications)
 	e.Logger.Fatal(e.Start(":3000"))
 }
